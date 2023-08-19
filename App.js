d646d4f808ce2873b3b124eb91dd86bfd29613fe
Thakur2204 <135164876+Thakur2204@users.noreply.github.com>
2023-08-19 19:11:04 +0530

    second commit

--- App.js
+++ App.js
@@ -1,6 +1,22 @@
+import { useReducer} from "react"
 import "./style.css"
-
+export const ACTIONS = {
+  ADD_DIGIT: "add-digit",
+  CHOOSE_OPERATION: "choose-operation",
+  CLEAR: "clear",
+  DELETE_DIGIT: "delete-digit",
+  EVALUATE: "evaluate",
+}
+function reducer(state,{type,payload}){
+  switch(type){
+    case ACTIONS.ADD_DIGIT:
+      return {
+        
+      }
+  }
+}
 function App(){
+  const [{curroperand,prevoperand,operation}, dispatch] = useReducer(reducer, {})
   return(
     <div className="cal-grid">
       
@@ -25,37 +41,37 @@ function App(){
         /
       </button>
       <button >
-        1
-      </button>
-      <button >
-        2
-      </button>
-      <button >
-        3
-      </button>
-      <button >
-        *
-      </button>
-      <button >
-        4
-      </button>
-      <button >
-        5
-      </button>
-      <button >
-        6
-      </button>
-      <button >
-        +
-      </button>
-      <button >
-        7
+        9
       </button>
       <button >
         8
       </button>
       <button >
-        9
+        7
+      </button>
+      <button >
+        *
+      </button>
+      <button >
+        6
+      </button>
+      <button >
+        5
+      </button>
+      <button >
+        4
+      </button>
+      <button >
+        +
+      </button>
+      <button >
+        3
+      </button>
+      <button >
+        2
+      </button>
+      <button >
+        1
       </button>
       <button >
         -
